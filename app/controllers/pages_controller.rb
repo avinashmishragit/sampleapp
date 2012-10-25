2f5257dbac3de2e5029cf87ffb6b2f4662a5b1bd
innocentavi <mishra.avinash35@gmail.com>
2012-10-25 11:56:08 +0530

    Added title to application.html.erb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,17 @@
 class PagesController < ApplicationController
   def contact
-
+    @title="Contact"
   end
 
   def home
-
+    @title="Home"
   
   end	
 
+  def about
+
+    @title="About"
   
+  end  
 
 end
