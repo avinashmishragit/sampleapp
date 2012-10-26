1ca5197f0b083be85017611ee84c2297e7b9917b
innocentavi <mishra.avinash35@gmail.com>
2012-10-26 16:32:35 +0530

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,19 @@ class UsersController < ApplicationController
 
   def new
   	@title="Sign up"
+  	@user=User.new
   end
+  def create
+
+    @user=User.new(params[:user])
+    if @user.save
+    	redirect_to @user
+    else
+      @title="sign up"
+      render 'new'	
+    end
+
+  end	
+
+
 end
